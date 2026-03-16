f554f86c1ff55b3599baf4eaf5d81c18b600f6da
thomasjeu <jeuthomas03@gmail.com>
2026-03-16 17:14:38 +1300

    Initial Commit

---
+++ ClaudeTrackerApp/ClaudeTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeTrackerAppApp.swift
+//  ClaudeTrackerApp
+//
+//  Created by Thomas Jeu on 16/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudeTrackerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClaudeTrackerApp
+//
+//  Created by Thomas Jeu on 16/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
